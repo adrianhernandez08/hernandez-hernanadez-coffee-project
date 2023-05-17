2afaeef5a76d5c96a5f3d754e3ab63b2b585d55a
Adrian Hernandez <adrian.c.hernandez08@gmail.com>
2023-05-17 15:11:50 -0500

    1511 commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 function renderCoffee(coffee) {
     var html = '<tr class="coffee">';
+    var html = '<div class="coffee d-block col-4 mx-auto p-auto">';
     html += '<td>' + coffee.id + '</td>';
     html += '<td>' + coffee.name + '</td>';
     html += '<td>' + coffee.roast + '</td>';
@@ -10,24 +11,53 @@ function renderCoffee(coffee) {
     return html;
 }
 
+//convert table to strings //
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for(var i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
+// export data into html tables //
 
+let tbody= document.querySelector("#coffees")
+let roastSelection = document.querySelector("#roast-selection");
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
+    var selectedRoast = roastSelection.value === "all";
     var filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
+        } else if (roastSelection.value === "all"){
+            filteredCoffees.push(coffee);
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+function searchCoffees(){
+    let searchRoast = searchBox.value.toUpperCase();
+    let filteredCoffees = [];
+    console.log(searchRoast);
+    coffees.forEach(function(coffee){
+        if (coffee.name.toUpperCase().includes(searchRoast)) {
+            filteredCoffees.push(coffee);
+            console.log(filteredCoffees);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+function addCoffees(){
+    let addID = coffees.length+1;
+    let addName = inputName.value.toString();
+    let addRoast = inputRoast.value.toString();
+    let addObject = {id:addID, name: addName, roast: addRoast};
+    coffees.push(addObject);
+    console.log(coffees);
+
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -52,6 +82,12 @@ var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
-
 submitButton.addEventListener('click', updateCoffees);
+}
+
+var searchBox = document.querySelector("#searchBox");
+searchBox.addEventListener("keyup", searchCoffees);
+var submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", updateCoffees);
+var addCoffeeButton = document.querySelector("#input-submit");
+addCoffeeButton.addEventListener("click", addCoffees)
